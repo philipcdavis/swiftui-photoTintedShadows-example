8ae2dd9013197c4244c682f0b5517e620ceea364
Philip Davis <philipcdavis11@gmail.com>
2021-01-13 10:07:03 -0500

    Initial Commit

---
+++ Glow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Glow
+//
+//  Created by Philip Davis on 1/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glow/GlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowApp.swift
+//  Glow
+//
+//  Created by Philip Davis on 1/13/21.
+//
+
+import SwiftUI
+
+@main
+struct GlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
